11ad2ee447e33071abb3b3f0300fb575cbeca45f
Niels Kristensen <niels@kristensen.io>
2015-06-20 13:32:15 +0200

    handle dhcpd and kea differently

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ CSV format: (look in samples)
     Column A - network - eg 10.1.0.0 or 10.1.0.0/24
     Column B - netmask - eg 255.255.255.0 or
     Column C - statics - eg 31
-    Column D - hosts/splitpercent - eg 60,30,10 (split between 3 hosts 60%,30%,10%)
+    Column D - dhcpware/splitpercent - eg dhcpd/60,kea/30,dhcpd/10 (split between 3 hosts 60%,30%,10%)
     Column E - gateway - eg 10.1.0.1
     Column F - leasetime - eg 10800
     Column G - pool options - eg failover peer "rf";
--- split-scope.rb
+++ split-scope.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'netaddr'
-require 'CSV'
+require 'csv'
 
 config = Hash.new {|h,k| h[k] = Array.new }
 
@@ -12,7 +12,7 @@ subnets.each do |subnet|
   subnet[0] - network - eg 10.1.0.0 or 10.1.0.0/24
   subnet[1] - netmask - eg 255.255.255.0 or
   subnet[2] - statics - eg 31
-  subnet[3] - hosts/splitpercent - eg 60,30,10 (split between 3 hosts 60%,30%,10%)
+  subnet[3] - dhcpware/splitpercent - eg dhcpd/60,kea/30,dhcpd/10 (split between 3 hosts 60%,30%,10%)
   subnet[4] - gateway - eg 10.1.0.1
   subnet[5] - leasetime - eg 10800
   subnet[6] - pool options - eg failover peer "rf";
@@ -38,58 +38,66 @@ subnets.each do |subnet|
   lowcount = Integer(subnet[2])
 
   # For each host calculate scope and build config
-  subnet[3].split(',').each_with_index do |prcnt, index|
+  subnet[3].split(',').each_with_index do |hostinfo, index|
+    type = hostinfo.split('/')[0]
+    prcnt = hostinfo.split('/')[1]
 
     # Config pr. host / pr. ip version
-    configarr = "#{index}-#{cidr.version}"
+    configarr = "#{index}-#{type}-v#{cidr.version}"
 
-    '''
-    subnet 10.1.4.0 netmask 255.255.255.0 {
-    subnet6 2a02:0108:0107:2003:0000:0000:0000:0000/64 {
-    '''
-    if cidr.version == 4
-      config["#{configarr}"].push('subnet ' + cidr.network + ' netmask ' + cidr.wildcard_mask + ' {')
-    else
-      config["#{configarr}"].push('subnet6 ' + String(cidr) + ' {')
-    end
+    # Build dhcpd config
+    if type == 'dhcpd'
 
-    '''
-      pool {
-        range 10.1.4.32 10.1.4.254;
-        failover peer "rf";
+      '''
+      subnet 10.1.4.0 netmask 255.255.255.0 {
+      subnet6 2a02:0108:0107:2003:0000:0000:0000:0000/64 {
+      '''
+      if cidr.version == 4
+        config["#{configarr}"].push('subnet ' + cidr.network + ' netmask ' + cidr.wildcard_mask + ' {')
+      else
+        config["#{configarr}"].push('subnet6 ' + String(cidr) + ' {')
+      end
+
+      '''
+        pool {
+          range 10.1.4.32 10.1.4.254;
+          failover peer "rf";
+        }
+      '''
+      config["#{configarr}"].push('  pool {')
+      # Last scope gets leftovers
+      if index == subnet[3].size - 1
+        highcount = ipcount - 1
+      else
+        highcount = (dhcppool / 100.0 * Integer(prcnt) + lowcount).floor
+      end
+      config["#{configarr}"].push('    range ' + cidr.nth(lowcount) + ' ' + cidr.nth(highcount) + ';')
+      lowcount = highcount + 1
+
+      config["#{configarr}"].push('    ' + String(subnet[6]))
+      config["#{configarr}"].push('  }')
+
+      '''
+        default-lease-time 604800;
+        max-lease-time 604800;
+        option routers 10.1.4.1;
+        option broadcast-address 10.1.4.255;
+        option subnet-mask 255.255.255.0;
+        option domain-name-servers 10.0.224.10, 10.0.225.10;
       }
-    '''
-    config["#{configarr}"].push('  pool {')
-    # Last scope gets leftovers
-    if index == subnet[3].size - 1
-      highcount = ipcount - 1
-    else
-      highcount = (dhcppool / 100.0 * Integer(prcnt) + lowcount).floor
+      '''
+      config["#{configarr}"].push('  default-lease-time ' + String(subnet[5]) + ';')
+      config["#{configarr}"].push('  max-lease-time ' + String(subnet[5]) + ';')
+      config["#{configarr}"].push('  option routers ' + String(subnet[4]) + ';')
+      config["#{configarr}"].push('  option broadcast-address ' + cidr.nth(ipcount) + ';')
+      config["#{configarr}"].push('  option subnet-mask ' + cidr.wildcard_mask + ';')
+      config["#{configarr}"].push('  ' + String(subnet[7]))
+      config["#{configarr}"].push('}')
+      config["#{configarr}"].push('')
+
+    # Build kea config
+    elsif type == 'kea'
     end
-    config["#{configarr}"].push('    range ' + cidr.nth(lowcount) + ' ' + cidr.nth(highcount) + ';')
-    lowcount = highcount + 1
-
-    config["#{configarr}"].push('    ' + String(subnet[6]))
-    config["#{configarr}"].push('  }')
-
-    '''
-      default-lease-time 604800;
-      max-lease-time 604800;
-      option routers 10.1.4.1;
-      option broadcast-address 10.1.4.255;
-      option subnet-mask 255.255.255.0;
-      option domain-name-servers 10.0.224.10, 10.0.225.10;
-    }
-    '''
-    config["#{configarr}"].push('  default-lease-time ' + String(subnet[5]) + ';')
-    config["#{configarr}"].push('  max-lease-time ' + String(subnet[5]) + ';')
-    config["#{configarr}"].push('  option routers ' + String(subnet[4]) + ';')
-    config["#{configarr}"].push('  option broadcast-address ' + cidr.nth(ipcount) + ';')
-    config["#{configarr}"].push('  option subnet-mask ' + cidr.wildcard_mask + ';')
-    config["#{configarr}"].push('  ' + String(subnet[7]))
-    config["#{configarr}"].push('}')
-    config["#{configarr}"].push('')
-
   end
 end
 
